8d7b3a87652ec8526848265b89bcef9513150a30
Peter Czigany <p.czigany@gmail.com>
2023-11-11 18:44:42 +0100

    return transfers between dates

--- src/main/java/com/peterczigany/transaction/TransferRepository.java
+++ src/main/java/com/peterczigany/transaction/TransferRepository.java
@@ -1,7 +1,11 @@
 package com.peterczigany.transaction;
 
 import com.peterczigany.transaction.model.Transfer;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TransferRepository extends CrudRepository<Transfer, UUID> {}
+public interface TransferRepository extends CrudRepository<Transfer, UUID> {
+  List<Transfer> findByLocalDateTimeBetween(LocalDateTime firstTime, LocalDateTime lastTime);
+}
