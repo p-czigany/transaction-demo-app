713ca4ba3630c6bda89a45f4c351c2b03fd00af2
Peter Czigany <p.czigany@gmail.com>
2023-11-11 23:53:27 +0100

    red: fix test case

--- src/test/java/com/peterczigany/transaction/TransferControllerTest.java
+++ src/test/java/com/peterczigany/transaction/TransferControllerTest.java
@@ -1,40 +1,32 @@
 package com.peterczigany.transaction;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.peterczigany.transaction.model.Transfer;
 import com.peterczigany.transaction.repository.TransferRepository;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class TransferControllerTest {
-  @MockBean private TransferRepository repository;
-
-  @Autowired private TransferController controller;
-
-  @Autowired private MockMvc mockMvc;
+  @Mock private TransferRepository repository;
+  @InjectMocks private TransferController controller;
 
   @Test
   void getTransfersBetweenTimes() throws Exception {
     List<Transfer> transfers = List.of(new Transfer(), new Transfer());
     Mockito.when(repository.findByLocalDateTimeBetween(any(), any())).thenReturn(transfers);
 
-    mockMvc
-        .perform(
-            get("/api/transactions/")
-                .param("firstTime", "2024-01-01T00:01")
-                .param("lastTime", "2024-01-02T00:00")
-                .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$", hasSize(2)));
+    assertThat(
+            controller.getTransactionsInTimeInterval(
+                LocalDateTime.parse("2024-01-01T00:01"), LocalDateTime.parse("2024-01-02T00:00")))
+        .isEqualTo(transfers);
   }
 }
