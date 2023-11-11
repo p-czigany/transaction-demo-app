97e5e3e0213cae40b87e5dcf6ae5e40865fdb9b7
Peter Czigany <p.czigany@gmail.com>
2023-11-11 16:17:24 +0100

    test: red - test creation of domain object

--- src/test/java/com/peterczigany/transaction/TransactionTest.java
+++ src/test/java/com/peterczigany/transaction/TransactionTest.java
@@ -1,5 +1,7 @@
 package com.peterczigany.transaction;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +12,6 @@ class TransactionTest {
     Transaction transaction =
         new Transaction(
             null, "Alice The Sender", "Bruce The Recipient", BigDecimal.valueOf(100000.0));
+    assertThat(transaction.getSender()).isEqualTo("Alice the Sender");
   }
 }
