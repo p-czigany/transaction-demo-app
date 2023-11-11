f18d781f181f01c72e048a412b2cf7ca9c55768c
Peter Czigany <p.czigany@gmail.com>
2023-11-11 16:14:07 +0100

    test: red - test creation of domain object

--- src/test/java/com/peterczigany/transaction/TransactionTest.java
+++ src/test/java/com/peterczigany/transaction/TransactionTest.java
@@ -1,11 +1,14 @@
 package com.peterczigany.transaction;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 
 class TransactionTest {
 
   @Test
   void create() {
-    Transaction transaction = new Transaction();
+    Transaction transaction =
+        new Transaction(
+            null, "Alice The Sender", "Bruce The Recipient", BigDecimal.valueOf(100000.0));
   }
 }
