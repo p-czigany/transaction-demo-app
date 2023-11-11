ef7e76280a87bb20a663a5f59ea6d608174320aa
Peter Czigany <p.czigany@gmail.com>
2023-11-11 16:28:27 +0100

    test: green - test creation of domain object

--- src/main/java/com/peterczigany/transaction/Transaction.java
+++ src/main/java/com/peterczigany/transaction/Transaction.java
@@ -1,6 +1,7 @@
 package com.peterczigany.transaction;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Transaction {
@@ -8,12 +9,15 @@ public class Transaction {
   private final String sender;
   private final String recipient;
   private final BigDecimal amount;
+  private final LocalDateTime localDateTime;
 
-  public Transaction(UUID id, String sender, String recipient, BigDecimal amount) {
+  public Transaction(
+      UUID id, String sender, String recipient, BigDecimal amount, LocalDateTime localDateTime) {
     this.id = id;
     this.sender = sender;
     this.recipient = recipient;
     this.amount = amount;
+    this.localDateTime = localDateTime;
   }
 
   public String getSender() {
