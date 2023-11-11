900f8d7db59598016fb50c40c4ee5ef85797e2b3
Peter Czigany <p.czigany@gmail.com>
2023-11-11 16:15:13 +0100

    test: green - test creation of domain object

--- src/main/java/com/peterczigany/transaction/Transaction.java
+++ src/main/java/com/peterczigany/transaction/Transaction.java
@@ -1,3 +1,18 @@
 package com.peterczigany.transaction;
 
-public class Transaction {}
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Transaction {
+  private UUID id;
+  private String sender;
+  private String recipient;
+  private BigDecimal amount;
+
+  public Transaction(UUID id, String sender, String recipient, BigDecimal amount) {
+    this.id = id;
+    this.sender = sender;
+    this.recipient = recipient;
+    this.amount = amount;
+  }
+}
