092a3662ba8184fc8205ff0158360b2d576c0088
Peter Czigany <p.czigany@gmail.com>
2023-11-11 15:59:25 +0100

    test: red - test creation of domain object

---
+++ src/test/java/com/peterczigany/transaction/TransactionTest.java
@@ -0,0 +1,11 @@
+package com.peterczigany.transaction;
+
+import org.junit.jupiter.api.Test;
+
+class TransactionTest {
+
+  @Test
+  void create() {
+    Transaction transaction = new Transaction();
+  }
+}
