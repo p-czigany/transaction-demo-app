3d68180d73a1566772ed4e9db7dcd741013ef0ca
Peter Czigany <p.czigany@gmail.com>
2023-11-11 23:58:26 +0100

    red: fix test case

--- src/integration-test/java/TransactionApplicationIntegrationTest.java
+++ src/integration-test/java/TransactionApplicationIntegrationTest.java
@@ -10,21 +10,14 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.MOCK,
-    classes = TransactionApplication.class)
-@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = TransactionApplication.class)
 @AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-integrationtest.properties")
 class TransactionApplicationIntegrationTest {
 
   @Autowired private MockMvc mockMvc;
