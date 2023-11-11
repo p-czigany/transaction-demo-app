761ee86bcd83f421f6da033620d5d05a897e544e
Peter Czigany <p.czigany@gmail.com>
2023-11-11 17:34:12 +0100

    test: red - repository looks for items in time range

--- src/test/java/com/peterczigany/transaction/TransactionRepositoryTest.java
+++ src/test/java/com/peterczigany/transaction/TransactionRepositoryTest.java
@@ -2,10 +2,14 @@ package com.peterczigany.transaction;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.peterczigany.transaction.model.Transaction;
+import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
 class TransactionRepositoryTest {
@@ -18,7 +22,11 @@ class TransactionRepositoryTest {
   }
 
   @Test
+  @Sql("/data-test.sql")
   void findAllBetweenGivenTimes() {
-    assertThat(repository.findByLocalDateTimeBetween(LocalDateTime firstTime, LocalDateTime lastTime);
+    List<Transaction> returnedTransactions =
+        findByLocalDateTimeBetween(
+            LocalDateTime.of(2024, 1, 2, 0, 0), LocalDateTime.of(2024, 1, 3, 0, 0));
+    assertThat(returnedTransactions).hasSize(2);
   }
 }
