88bd0ce7cb7b7dba0b8b2cb691c12df379645e4e
Peter Czigany <p.czigany@gmail.com>
2023-11-11 13:58:47 +0100

    refactor: checking testing framework

--- src/test/java/com/peterczigany/transaction/SomeBasicTest.java
+++ src/test/java/com/peterczigany/transaction/SomeBasicTest.java
@@ -2,10 +2,10 @@ package com.peterczigany.transaction;
 
 import org.junit.jupiter.api.Test;
 
-public class SomeBasicTest {
+class SomeBasicTest {
 
   /** Checks the testing framework. Method empty by design. */
   @Test
   @SuppressWarnings("java:S2699")
-  public void emptyTestMethod() {}
+  void emptyTestMethod() {}
 }
