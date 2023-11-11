001aaa7b3ae1ba205a5ca54b084169f40c98bfe2
Peter Czigany <p.czigany@gmail.com>
2023-11-11 13:58:08 +0100

    doc: checking testing framework

--- src/test/java/com/peterczigany/transaction/SomeBasicTest.java
+++ src/test/java/com/peterczigany/transaction/SomeBasicTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 public class SomeBasicTest {
 
+  /** Checks the testing framework. Method empty by design. */
   @Test
+  @SuppressWarnings("java:S2699")
   public void emptyTestMethod() {}
 }
