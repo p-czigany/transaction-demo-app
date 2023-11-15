928d124407d7749f7e96d3dcd55ad14a8e0ce01b
Peter Czigany <p.czigany@gmail.com>
2023-11-15 01:20:37 +0100

    finalize printed version

--- src/main/java/com/peterczigany/transaction/initproject/InitProject.java
+++
@@ -1,9 +0,0 @@
-package com.peterczigany.transaction.initproject;
-
-public class InitProject {
-
-  private InitProject() {}
-
-  //  @SuppressWarnings("java:S3400")
-  public static String OK = "OK";
-}
